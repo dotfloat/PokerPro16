d2c68dd553917574c8f688eb3cafa5436f5158ab
Kjetil Kjørstad <kkj004@student.uib.no>
2016-03-02 23:44:04 +0100

    Kjetil: Testet ANT på Eclipse ( OK etter korrigering av JAVA_HOME-error ) Committer også endring i kode, med ignore på Eclipse .project-fil...

--- src/org/uib/inf112_g2/TestApp.java
+++ src/org/uib/inf112_g2/TestApp.java
@@ -2,6 +2,8 @@ package org.uib.inf112_g2;
 
 public class TestApp {
 	static public void main(String[] args) {
-		System.out.println("Hello, world!");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Hello, world!");
+		}
 	}
 }
