692ab45efc738ae54cc933f14275f10cb36b8e2d
INF112v16-mna011 <miknau94@gmail.com>
2016-03-09 15:22:49 +0100

    Added PlayerAction interface for the backend to ask the user or AI for action (check, fold, etc)

---
+++ src/main/java/org/gruppe2/backend/PlayerAction.java
@@ -0,0 +1,12 @@
+package org.gruppe2.backend;
+
+/**
+ * Created by Mikal on 09.03.16.
+ */
+public interface PlayerAction {
+
+    /**
+     * Asks for an action from AI or the user (GUI)
+     */
+    public void doAction();
+}
