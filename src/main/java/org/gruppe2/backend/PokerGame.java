12c0c11bdb9564b983257f0a6056884be4fbb9f9
INF112v16-xix004 <xix004@student.uib.no>
2016-03-09 13:59:47 +0100

    PokerGame will become main class for running a pokergame

---
+++ src/main/java/org/gruppe2/backend/PokerGame.java
@@ -0,0 +1,12 @@
+package org.gruppe2.backend;
+
+/**
+ * Created by Åsmund on 09/03/2016.
+ */
+public class PokerGame {
+
+    
+    public static void main(String[] args) {
+        System.out.println("dette blir det beste spillet");
+    }
+}
