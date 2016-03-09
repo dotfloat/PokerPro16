bf2e0275785b518e69ca91528b4b40e20b2b4554
INF112v16-mna011 <miknau94@gmail.com>
2016-03-09 15:34:47 +0100

    Added chips to player and get functions

--- src/main/java/org/gruppe2/backend/Player.java
+++ src/main/java/org/gruppe2/backend/Player.java
@@ -5,8 +5,18 @@ package org.gruppe2.backend;
  */
 public class Player {
     private String name;
+    private int chips;
 
-    public Player(String name) {
+    public Player(String name, int startChips) {
         this.name = name;
+        this.chips = startChips;
+    }
+
+    public int getChips() {
+        return chips;
+    }
+
+    public String getName(){
+        return name;
     }
 }
