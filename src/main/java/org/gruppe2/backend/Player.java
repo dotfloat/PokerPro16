5f1773a6346a2fcfbcc6fe4474c21e87c4b227ea
INF112v16-mna011 <miknau94@gmail.com>
2016-03-09 13:46:23 +0100

    Added very basic player class

---
+++ src/main/java/org/gruppe2/backend/Player.java
@@ -0,0 +1,12 @@
+package org.gruppe2.backend;
+
+/**
+ * Created by Mikal on 09.03.16.
+ */
+public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
