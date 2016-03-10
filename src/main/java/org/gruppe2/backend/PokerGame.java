a52b62e4c901bc2fcf95168f60558e8720d33703
INF112v16-xix004 <xix004@student.uib.no>
2016-03-10 14:05:52 +0100

    removed main-method

--- src/main/java/org/gruppe2/backend/PokerGame.java
+++ src/main/java/org/gruppe2/backend/PokerGame.java
@@ -14,49 +14,28 @@ public class PokerGame{
     int bigBlind;
 
     private ArrayList<Player> players;
-    
-    // Skal ikke stå slik, er bare for foreløpig oversikt
-    public ArrayList<Player> getPlayers() {
-		return players;
-	}
+    private PokerTable table;
+
+
     //need pokerTable CEM PLS 
     
     
     
-    public PokerGame(GUI gui, ArrayList<Player> players/*, PokerTable table*/){
+    public PokerGame(GUI gui){
         this.gui = gui;
-        this.players = players;
-
+        // PokerTable = new PokerTable(new Deck());
         //need poker table and be able give players to board, and board to players
     }
 
-    //and many more methods
+    
+
+    // Skal ikke stå slik, er bare for foreløpig oversikt
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
 
 
     
 
-	public static void main(String[] args) {
-    	
-        System.out.println("dette blir det beste spillet");
-        
-        GUI playerGUI = new GUI();
-        
-        ArrayList<Player> currentPlayers = new ArrayList<Player>();
-        currentPlayers.add(new Player("First", 500));
-        currentPlayers.add(new Player("Second", 500));
-        
-        PokerGame currentHand = new PokerGame(playerGUI, currentPlayers/*, playingTable*/);
-        
-        boolean gameDone = false;
-        while (!gameDone) {
-			
-        	for (Player player : currentHand.getPlayers()) {
-				player.doAction();				
-			}
-        	
-        	System.out.println("The Ride never ends.");
-        	
-		}
-        
-    }
+
 }
