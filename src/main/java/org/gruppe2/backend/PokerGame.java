a973c6ca0f2b949b325fb9ac4d940febe728840d
INF112v16-aom035 <aom035@student.uib.no>
2016-03-10 12:44:23 +0100

    Added game loop and tweaked fields and constructor, minor main stuff

--- src/main/java/org/gruppe2/backend/PokerGame.java
+++ src/main/java/org/gruppe2/backend/PokerGame.java
@@ -14,31 +14,49 @@ public class PokerGame{
     int bigBlind;
 
     private ArrayList<Player> players;
-
-    //need pokerTable
-
-    public PokerGame(GUI gui, ArrayList<Player> players, int smallBlind, int bigBlind){
+    
+    // Skal ikke stå slik, er bare for foreløpig oversikt
+    public ArrayList<Player> getPlayers() {
+		return players;
+	}
+    //need pokerTable CEM PLS 
+    
+    
+    
+    public PokerGame(GUI gui, ArrayList<Player> players/*, PokerTable table*/){
         this.gui = gui;
         this.players = players;
-        this.smallBlind = smallBlind;
-        this.bigBlind = bigBlind;
 
         //need poker table and be able give players to board, and board to players
-    }
-
-    //todo
-    public void addPlayer(Player player){
-        players.add(player);
-    }
-
-    //todo
-    public void createPokerTable(){
     }
 
     //and many more methods
 
 
-    public static void main(String[] args) {
+    
+
+	public static void main(String[] args) {
+    	
         System.out.println("dette blir det beste spillet");
+        
+        GUI playerGUI = new GUI();
+        
+        ArrayList<Player> currentPlayers = new ArrayList<Player>();
+        currentPlayers.add(new Player("First", 500));
+        currentPlayers.add(new Player("Second", 500));
+        
+        PokerGame currentHand = new PokerGame(playerGUI, currentPlayers/*, playingTable*/);
+        
+        boolean gameDone = false;
+        while (!gameDone) {
+			
+        	for (Player player : currentHand.getPlayers()) {
+				player.doAction();				
+			}
+        	
+        	System.out.println("The Ride never ends.");
+        	
+		}
+        
     }
 }
