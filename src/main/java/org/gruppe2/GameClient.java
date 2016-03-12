caee10f6d6c90b30941931f3c074ae6a1d58d8d3
Zohar Malamant <dotfloat@gmail.com>
2016-03-12 15:34:23 +0100

    Add better javadoc for GameClient

--- src/main/java/org/gruppe2/GameClient.java
+++ src/main/java/org/gruppe2/GameClient.java
@@ -26,20 +26,52 @@ public interface GameClient {
     /* Game events */
 
     /**
-     *
+     * Called after the round starts
      * @param board
      */
     default void onRoundStart(Board board) {}
+
+    /**
+     * Called before the round ends
+     * @param board
+     */
     default void onRoundEnd(Board board) {}
 
-    default Action onTurn(Board board) { return null; }
+    /**
+     * Called when it's the player's action. It is allowed to block.
+     *
+     * Only active players can receive this. (Not spectators)
+     *
+     * @param board
+     * @return The action that the client wants to do
+     */
+    default Action onTurn(Board board) { return Action.DISCONNECT; }
+
+    /**
+     * Called before the player has decided on an action.
+     * @param board
+     * @param otherPlayer
+     */
     default void onOtherPlayerTurn(Board board, Player otherPlayer) {}
+
+    /**
+     * Called after the player has decided on an action.
+     * @param board
+     * @param otherPlayer
+     * @param action
+     */
     default void onOtherPlayerAction(Board board, Player otherPlayer, Action action) {}
 
+    /**
+     * Called when cards are added to the communal cards.
+     * @param board
+     * @param newCards
+     */
     default void onCommunalCards(Board board, List<Card> newCards) {}
 }
 
-class Action {
+enum Action {
+    DISCONNECT
 }
 
 class Board {
