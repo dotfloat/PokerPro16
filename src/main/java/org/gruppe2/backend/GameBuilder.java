1a564992c2a25f201d51a1ce01a4db776322e74f
Zohar Malamant <dotfloat@gmail.com>
2016-03-16 13:36:05 +0100

    Add basic GameBuilder

---
+++ src/main/java/org/gruppe2/backend/GameBuilder.java
@@ -0,0 +1,38 @@
+package org.gruppe2.backend;
+
+public class GameBuilder {
+    private int numAI = 0;
+    private GameClient client = null;
+    private int bigBlind = 0;
+    private int smallBlind = 0;
+    private int startMoney = 0;
+
+    public GameBuilder ai(int numAI) {
+        this.numAI = numAI;
+
+        return this;
+    }
+
+    public GameBuilder mainClient(GameClient client) {
+        this.client = client;
+
+        return this;
+    }
+
+    public GameBuilder blinds(int bigBlind, int smallBlind) {
+        this.bigBlind = bigBlind;
+        this.smallBlind = smallBlind;
+
+        return this;
+    }
+
+    public GameBuilder startMoney(int startMoney) {
+        this.startMoney = startMoney;
+
+        return this;
+    }
+
+    public GameSession build() {
+        return null;
+    }
+}
