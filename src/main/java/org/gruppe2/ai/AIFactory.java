9809122dc026cca7beefb14919990d5d07e1b4fb
INF112v16-rse005 <rse005@student.uib.no>
2016-03-16 17:38:39 +0100

    Implemented AIFactory

--- src/main/java/org/gruppe2/ai/AIFactory.java
+++ src/main/java/org/gruppe2/ai/AIFactory.java
@@ -1,5 +1,22 @@
 package org.gruppe2.ai;
 
+import org.gruppe2.backend.GameClient;
+
 public class AIFactory {
     private Difficulty difficulty;
+    
+    public AIFactory(Difficulty difficulty) {
+    	this.difficulty = difficulty;
+    }
+    
+    public GameClient generateBot() {
+    	switch(difficulty) {
+    	case EASY:
+    	case MEDIUM:
+    	case HARD:
+    	case RANDOM:
+    	default:
+    		return new AIClient();
+    	}
+    }
 }
