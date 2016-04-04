4e13bf4dd23c6dcd71b1d24f20067cfa4fe81001
Zohar Malamant <dotfloat@gmail.com>
2016-04-04 06:05:02 +0200

    Move FXML loading boilerplate out of the CloseMe controller

--- src/main/java/org/gruppe2/ui/javafx/CloseMe.java
+++ src/main/java/org/gruppe2/ui/javafx/CloseMe.java
@@ -2,26 +2,8 @@ package org.gruppe2.ui.javafx;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.layout.VBox;
 
-import java.io.IOException;
-
-class CloseMe extends VBox {
-    CloseMe() {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
-                    "/views/" + getClass().getSimpleName() + ".fxml"));
-
-            fxmlLoader.setController(this);
-            fxmlLoader.setRoot(this);
-
-            fxmlLoader.load();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
+class CloseMe extends VBoxController {
     @FXML
     public void quit() {
         Platform.exit();
---
+++ src/main/java/org/gruppe2/ui/javafx/VBoxController.java
@@ -0,0 +1,23 @@
+package org.gruppe2.ui.javafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
+class VBoxController extends VBox {
+    VBoxController() {
+        /* Java doesn't have templates so we have to resort to writing boilerplate code -.-  */
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
+                    "/views/" + getClass().getSimpleName() + ".fxml"));
+
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
