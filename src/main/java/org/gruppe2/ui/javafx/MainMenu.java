722c9f6605c0c65c3232379ed526d96ab8cf89bd
INF112v16-xix004 <xix004@student.uib.no>
2016-04-05 14:43:11 +0200

    created basic MainMenu.java for the javafx

---
+++ src/main/java/org/gruppe2/ui/javafx/MainMenu.java
@@ -0,0 +1,13 @@
+package org.gruppe2.ui.javafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class MainMenu extends BorderPaneController {
+
+}
