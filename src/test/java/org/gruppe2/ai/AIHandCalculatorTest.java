d54f7b7aaeecdbc12e5b4b942cc540f6350d61be
Daniel Jensen <dje004@uib.no>
2016-04-06 21:27:03 +0200

    HandCalculator now calculate the winning probability with bruteforce using the old ShowdownEvaluator

---
+++ src/test/java/org/gruppe2/ai/AIHandCalculatorTest.java
@@ -0,0 +1,24 @@
+package org.gruppe2.ai;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.gruppe2.game.old.Deck;
+import org.gruppe2.game.old.GameClient;
+import org.gruppe2.game.old.Player;
+import org.gruppe2.game.old.Table;
+import org.junit.Test;
+
+public class AIHandCalculatorTest {
+
+	@Test
+	public void test() {
+		AIMockTable t = new AIMockTable();
+		AIMockPlayer player = new AIMockPlayer("player");
+		player.setCards(t.drawACard(), t.drawACard());
+		double d = AIHandCalculator.getHandStrength(t, player);
+		System.out.println(d);
+	}
+
+}
