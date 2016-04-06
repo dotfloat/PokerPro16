9ebf23fa54179bd50fb097d47f3a184588ebe470
Kjetil Kjørstad <kkj004@student.uib.no>
2016-04-06 14:26:51 +0200

    Just reset push

--- src/main/java/org/gruppe2/ui/javafx/BorderPaneController.java
+++ src/main/java/org/gruppe2/ui/javafx/BorderPaneController.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by kjors on 04.04.2016.
@@ -12,13 +13,24 @@ import java.io.IOException;
 public class BorderPaneController extends BorderPane {
 
     BorderPaneController() {
+
+//        URL url = getClass().getResource("/views/" + getClass().getSimpleName() + ".fxml");
+//        SceneController.setScene( url );
+
+
+
         try {
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(
                     "/views/" + getClass().getSimpleName() + ".fxml"));
 
-            fxmlLoader.setController(this);
-            fxmlLoader.setRoot(this);
- 
+            if (fxmlLoader.getController() == null) {
+                fxmlLoader.setController(this);
+            }
+
+            if (fxmlLoader.getRoot() == null) {
+                fxmlLoader.setRoot(this);
+            }
+
             this.setStyle("-fx-background-color: black");
 
             fxmlLoader.load();
