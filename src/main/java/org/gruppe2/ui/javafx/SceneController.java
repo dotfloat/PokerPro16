351c461fe496925cc65b9f23f71b3b23bd6f4292
Kjetil Kjørstad <kkj004@student.uib.no>
2016-04-06 13:29:12 +0200

    Added scene controller with method setScene(URL)

---
+++ src/main/java/org/gruppe2/ui/javafx/SceneController.java
@@ -0,0 +1,27 @@
+package org.gruppe2.ui.javafx;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+import java.net.URL;
+
+/**
+ * Created by kjors on 06.04.2016.
+ *
+ * Changes scene in PokerApplication main root BorderPane
+ * #url = URL to fxml-file
+ */
+public class SceneController {
+
+
+    public static void setScene(URL url) {
+
+        try {
+            BorderPane newScene = FXMLLoader.load( url );
+            BorderPane stage = PokerApplication.getRoot();
+            stage.setCenter( newScene );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
