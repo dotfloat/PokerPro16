d98ce267c9b85dbff622fae8e55e8aa379e40456
Daniel <Daniel@129.177.47.104>
2016-04-06 14:53:34 +0200

    Added calculations for each hand

---
+++ src/main/java/org/gruppe2/ai/CalculateFullHouse.java
@@ -0,0 +1,22 @@
+package org.gruppe2.ai;
+
+import java.util.ArrayList;
+
+import org.gruppe2.game.old.Card;
+
+public class CalculateFullHouse {
+	public static double getChanceForFullHouse(ArrayList<Card> currentCards, int cardsLeftToPick, int numberOfActivePlayers){
+		//TODO
+		return 0;
+	}
+	
+	public static double getChanceOfWinningWithFullHouse (ArrayList<Card> currentCards,int cardsLeftToPick, int numberOfActivePlayers){
+		//TODO
+		return 0;
+	}
+	
+	private ArrayList<Card> getMissingCardsForFullHouse(ArrayList<Card> currentCards){
+		//TODO
+		return null;
+	}
+}
---
+++ src/main/java/org/gruppe2/ai/CalculateStraightFlush.java
@@ -0,0 +1,22 @@
+package org.gruppe2.ai;
+
+import java.util.ArrayList;
+
+import org.gruppe2.game.old.Card;
+
+public class CalculateStraightFlush {
+	public static double getChanceForStraightFlush(ArrayList<Card> currentCards, int cardsLeftToPick, int numberOfActivePlayers){
+		//TODO
+		return 0;
+	}
+	
+	public static double getChanceOfWinningWithStraightFlush (ArrayList<Card> currentCards,int cardsLeftToPick, int numberOfActivePlayers){
+		//TODO
+		return 0;
+	}
+	
+	private ArrayList<Card> getMissingCardsForStraightFlush(ArrayList<Card> currentCards){
+		//TODO
+		return null;
+	}
+}
