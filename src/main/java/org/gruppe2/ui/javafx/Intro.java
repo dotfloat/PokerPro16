b58609511defc8d2987558d3e495276e80ec24f2
Petter <petterthorgersen@hotmail.com>
2016-04-06 16:04:30 +0200

    now launches in intro and can go to menu

--- src/main/java/org/gruppe2/ui/javafx/Intro.java
+++ src/main/java/org/gruppe2/ui/javafx/Intro.java
@@ -6,44 +6,40 @@ package org.gruppe2.ui.javafx;
 
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
-import org.gruppe2.game.event.Event;
 
-import javafx.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
 
 public class Intro extends BorderPaneController {
 
     @FXML private ImageView logo;
     private DoubleProperty fitWidth;
-    Stage prevStage;
 
     public Intro(){
         super();
         fitWidth = new SimpleDoubleProperty();
-        logo.fitWidthProperty().bind(fitWidth.multiply(0.8));
+//        logo.fitWidthProperty().bind(fitWidth.multiply(0.8));
     }
 
     public void goToMenu(ActionEvent event) throws IOException{
         System.out.println("Something happens.");
-        Parent menuParent = FXMLLoader.load(getClass().getResource("/views/MainMenu.fxml"));
-        Scene menuScene = new Scene(menuParent);
+        SceneController.setScene((getClass().getResource("/views/MainMenu.fxml")));
+        /*
+        Parent root = FXMLLoader.load(getClass().getResource("/views/MainMenu.fxml"));
         Stage menuStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene menuScene = new Scene(root);
         menuStage.setScene(menuScene);
         menuStage.show();
-
+        */
+        //BorderPane borderPane = PokerApplication.getRoot();
+        //borderPane.setCenter(new BorderPane(FXMLLoader.load(getClass().getResource("/views/MainMenu.fxml"))));
     }
 
     public DoubleProperty fitWidthProperty() {
         return fitWidth;
-    }
-    private void setPrevStage(Stage stage){
-        prevStage = stage;
     }
 }
