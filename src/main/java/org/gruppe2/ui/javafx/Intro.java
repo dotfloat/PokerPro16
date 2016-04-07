95369edf75b8bbde5d71b6626a97854872164998
Petter <petterthorgersen@hotmail.com>
2016-04-07 11:45:25 +0200

    some transitions and scaling

--- src/main/java/org/gruppe2/ui/javafx/Intro.java
+++ src/main/java/org/gruppe2/ui/javafx/Intro.java
@@ -5,11 +5,16 @@ package org.gruppe2.ui.javafx;
  */
 
 import javafx.animation.FadeTransition;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -30,12 +35,26 @@ public class Intro implements Initializable {
         assert logo != null : "is null";
         logo.fitWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.8));
 
-        FadeTransition fadeTransition = new FadeTransition(Duration.millis(700), clickToContinue);
+        clickToContinueFading(clickToContinue);
+
+    }
+
+    private void fallingAnimation(Rectangle node){
+        final Timeline timeline = new Timeline();
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.setAutoReverse(true);
+        final KeyValue kv = new KeyValue(node.yProperty(), 500);
+        final KeyFrame kf = new KeyFrame(Duration.millis(4000), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.play();
+    }
+
+    private void clickToContinueFading(Node node){
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(700), node);
         fadeTransition.setFromValue(1.0);
         fadeTransition.setToValue(0.1);
         fadeTransition.setCycleCount(Timeline.INDEFINITE);
         fadeTransition.setAutoReverse(true);
         fadeTransition.play();
-
     }
 }
--- src/main/java/org/gruppe2/ui/javafx/MainMenu.java
+++ src/main/java/org/gruppe2/ui/javafx/MainMenu.java
@@ -1,12 +1,15 @@
 package org.gruppe2.ui.javafx;
 
 
+import javafx.animation.FadeTransition;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-
+import javafx.util.Duration;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -22,17 +25,29 @@ public class MainMenu implements Initializable {
     @FXML private Button settings;
     @FXML private VBox vBox;
 
+    public void goToSinglePlayer() throws InterruptedException {
+        SceneController.setScene(getClass().getResource("/views/GameWindow.fxml"));
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         logo.fitWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.8));
         vBox.maxWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.3));
 
+        setButtonSize();
+        fadeIn();
+    }
+    private void fadeIn(){
+        FadeTransition fadeTransition = new FadeTransition(Duration.millis(800), vBox);
+        fadeTransition.setFromValue(0.0);
+        fadeTransition.setToValue(1.0);
+        fadeTransition.play();
+    }
+    private void setButtonSize(){
         createTable.setMaxWidth(Double.MAX_VALUE);
         joinTable.setMaxWidth(Double.MAX_VALUE);
         singlePlayer.setMaxWidth(Double.MAX_VALUE);
         viewStatistics.setMaxWidth(Double.MAX_VALUE);
         settings.setMaxWidth(Double.MAX_VALUE);
-
     }
 }
