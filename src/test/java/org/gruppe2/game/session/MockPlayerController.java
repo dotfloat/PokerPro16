a1f54816b28d825bc50b146b4ab3acc63f77e76b
INF112v16-htj063 <htj063@student.uib.no>
2016-04-11 09:37:46 +0200

    2 sessions lacked unimplemented methods

--- src/test/java/org/gruppe2/game/session/MockPlayerController.java
+++ src/test/java/org/gruppe2/game/session/MockPlayerController.java
@@ -1,6 +1,13 @@
 package org.gruppe2.game.session;
 
 import org.gruppe2.game.controller.AbstractPlayerController;
+import org.gruppe2.game.old.Action;
 
 public class MockPlayerController extends AbstractPlayerController {
+
+	@Override
+	public Action pollAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/test/java/org/gruppe2/game/session/TestSession.java
+++ src/test/java/org/gruppe2/game/session/TestSession.java
@@ -1,6 +1,7 @@
 package org.gruppe2.game.session;
 
 import org.gruppe2.game.GameState;
+import org.gruppe2.game.controller.AbstractPlayerController;
 
 public class TestSession extends Session {
     private GameState state = GameState.WAITING_FOR_PLAYERS;
@@ -9,4 +10,28 @@ public class TestSession extends Session {
     GameState getGameState() {
         return state;
     }
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean addPlayer(AbstractPlayerController controller) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void addPlayerAsync(AbstractPlayerController controller) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exit() {
+		// TODO Auto-generated method stub
+		
+	}
 }
