a64f518bc273e33fa24fb78a6b4a18d30d90d8fd
INF112v16-htj063 <htj063@student.uib.no>
2016-04-11 10:46:46 +0200

    merge fix

--- src/main/java/org/gruppe2/ui/javafx/GUIPlayerController.java
+++ src/main/java/org/gruppe2/ui/javafx/GUIPlayerController.java
@@ -1,9 +1,15 @@
 package org.gruppe2.ui.javafx;
 
 import org.gruppe2.game.controller.AbstractPlayerController;
+import org.gruppe2.game.model.PlayerModel;
 import org.gruppe2.game.old.Action;
 
 public class GUIPlayerController extends AbstractPlayerController {
+
+	public GUIPlayerController(PlayerModel model) {
+		super(model);
+		// TODO Auto-generated constructor stub
+	}
 
 	@Override
 	public Action pollAction() {
