8c35c1581b9a70dfffa9f7b5ed7fd9088bedf7ea
INF112v16-htj063 <htj063@student.uib.no>
2016-04-11 12:39:05 +0200

    fixing error in fxml view

--- src/main/java/org/gruppe2/ui/Resources.java
+++ src/main/java/org/gruppe2/ui/Resources.java
@@ -8,7 +8,9 @@ import javafx.scene.Node;
 public class Resources {
 	public static void loadFXML(Node node){
 		try {
-			FXMLLoader fxmlLoader = new FXMLLoader(Resources.class.getResource("/views/" + node.getClass().getSimpleName().replaceFirst("Controller", "") + ".fxml"));
+			String name = "/views/" + node.getClass().getSimpleName() + ".fxml";
+			System.out.println(name);
+			FXMLLoader fxmlLoader = new FXMLLoader(Resources.class.getResource(name));
 			
 			fxmlLoader.setRoot(node);
 			fxmlLoader.setController(node);
