7a8e60b40fd07c5eaaeb2a93e0f447979e465ac2
Petter <petterthorgersen@hotmail.com>
2016-04-11 16:00:08 +0200

    adding modal view for lobby whilst ingame

---
+++ src/main/java/org/gruppe2/ui/javafx/Lobby.java
@@ -0,0 +1,37 @@
+package org.gruppe2.ui.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+
+
+/**
+ * Created by Petter on 11/04/2016.
+ */
+public class Lobby extends BorderPane {
+
+    @FXML private TextField search;
+    @FXML private Button submit;
+    @FXML private CheckBox checkBoxFriends;
+    @FXML private TilePane lobbyTile;
+
+    public Lobby(){
+        search.prefWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.3));
+        submit.prefWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.1));
+        submit.setMaxWidth(Double.MAX_VALUE);
+    }
+
+    public void search(){
+        System.out.print("Searching");
+        lobbyTile.getChildren().add(new Label("Searching"));
+    }
+
+    public void friendBox(){
+        System.out.print("Displaying only friends");
+        lobbyTile.getChildren().add(new Label("Displaying table with friends"));
+    }
+}
