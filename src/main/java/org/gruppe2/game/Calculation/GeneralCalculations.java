eb01c27f274c75d76cab4a7e4dce288c4d818486
INF112v16-xix004 <xix004@student.uib.no>
2016-04-12 14:44:59 +0200

    implemented the HandCalculation interface in the classes

--- src/main/java/org/gruppe2/game/Calculation/GeneralCalculations.java
+++ src/main/java/org/gruppe2/game/Calculation/GeneralCalculations.java
@@ -4,14 +4,15 @@ import org.gruppe2.game.old.Card;
 import org.gruppe2.game.old.Player;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
- * Created by Åsmund on 11/04/2016.
+ * Created by ï¿½smund on 11/04/2016.
  */
 public class GeneralCalculations {
 
-    public static ArrayList<Integer> recurringFaceValues(ArrayList<Card> communityCards, Player p) {
+    public static ArrayList<Integer> recurringFaceValues(Collection<Card> communityCards, Player p) {
         ArrayList<Integer> recurringFaceValues = new ArrayList<Integer>();
 
         ArrayList<Card> allCards = new ArrayList<Card>(communityCards);
@@ -45,7 +46,7 @@ public class GeneralCalculations {
 
 
 
-    public static int amountOfSameFace(ArrayList<Card> communityCards, Player p){
+    public static int amountOfSameFace(Collection<Card> communityCards, Player p){
         HashMap<Integer, Integer> amountCards = new HashMap<Integer, Integer>();
         int amountOfSameKind = 1;
 
--- src/main/java/org/gruppe2/game/Calculation/ThreeOfAKind.java
+++ src/main/java/org/gruppe2/game/Calculation/ThreeOfAKind.java
@@ -5,14 +5,15 @@ import org.gruppe2.game.old.Player;
 import org.gruppe2.ui.javafx.CommunityCards;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
- * Created by Åsmund on 11/04/2016.
+ * Created by ï¿½smund on 11/04/2016.
  */
-public class ThreeOfAKind {
+public class ThreeOfAKind implements HandCalculation{
 
-    public static boolean canGetThreeOfAKind(ArrayList<Card> communityCards, Player p){
+    public static boolean canGetThreeOfAKind(Collection<Card> communityCards, Player p){
         int amountOfSameFace = GeneralCalculations.amountOfSameFace(communityCards,p);
 
         if(amountOfSameFace >= 3)
@@ -27,5 +28,15 @@ public class ThreeOfAKind {
         }
 
         return false;
+    }
+
+    @Override
+    public boolean canGetHand(Collection<Card> communityCards, Player p) {
+        return canGetThreeOfAKind(communityCards, p);
+    }
+
+    @Override
+    public double handProbability(Collection<Card> communityCards, Player p) {
+        return 0;
     }
 }
