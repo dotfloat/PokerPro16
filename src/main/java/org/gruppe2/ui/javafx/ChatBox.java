d9480898b234e20464b7323bb85f76b3f4c9104c
INF112v16-htj063 <htj063@student.uib.no>
2016-04-12 16:01:50 +0200

    merge new pictures

--- src/main/java/org/gruppe2/ui/javafx/ChatBox.java
+++ src/main/java/org/gruppe2/ui/javafx/ChatBox.java
@@ -1,45 +1,46 @@
 package org.gruppe2.ui.javafx;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-
 
 import org.gruppe2.ui.Resources;
 
 public class ChatBox extends TextArea {
 
+	public ChatBox() {
+		Resources.loadFXML(this);
+		setPositionsAndSettings();
+	}
 
+	private void setPositionsAndSettings() {
+		double chatScale = 0.22;
+		this.prefWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(chatScale));
+		this.prefHeightProperty()
+				.bind(PokerApplication.getRoot().heightProperty()
+						.multiply(chatScale));
+		this.maxWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(chatScale));
+		this.maxHeightProperty()
+				.bind(PokerApplication.getRoot().heightProperty()
+						.multiply(chatScale));
 
+		editableProperty().setValue(false);
+		setWrapText(true);
+	}
 
-    public ChatBox() {
-        Resources.loadFXML(this);
-        setPositionsAndSettings();
-    }
+	public void setEventListeners(TextField textField) {
 
-
-    private void setPositionsAndSettings() {
-        double chatScale = 0.22;
-        this.prefWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(chatScale));
-        this.prefHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(chatScale));
-        this.maxWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(chatScale));
-        this.maxHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(chatScale));
-
-        editableProperty().setValue(false);
-        setWrapText(true);
-
-    }
-
-    public void setEventListeners(TextField textField) {
-
-        textField.setOnAction(e -> { // Put text from textField to textArea
-            if (textField.getText().equals(null) || textField.getText().equals("")) setScrollTop(Double.MAX_VALUE);
-            else {
-                this.setText(this.getText() + "\n" + "General" + ": " + textField.getText());
-                this.setScrollTop(Double.MAX_VALUE);
-                textField.setText("");
-            }
-        });
-    }
+		textField.setOnAction(e -> { // Put text from textField to textArea
+					if (textField.getText().equals(null)
+							|| textField.getText().equals(""))
+						setScrollTop(Double.MAX_VALUE);
+					else {
+						this.setText(this.getText() + "\n" + "General" + ": "
+								+ textField.getText());
+						this.setScrollTop(Double.MAX_VALUE);
+						textField.setText("");
+					}
+				});
+	}
 }
--- src/main/java/org/gruppe2/ui/javafx/ChoiceBar.java
+++ src/main/java/org/gruppe2/ui/javafx/ChoiceBar.java
@@ -3,7 +3,6 @@ package org.gruppe2.ui.javafx;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
@@ -20,28 +19,35 @@ public class ChoiceBar extends HBox {
 	private int height = PokerApplication.getHeight();
 	private GUIPlayer client;
 	private Player player;
-	@FXML private TextField chatField;
-	@FXML private Button FOLD;
-	@FXML private Slider slider;
-	@FXML private Label sliderValue;
-	@FXML private Button BET;
-	
+	@FXML
+	private TextField chatField;
+	@FXML
+	private Button FOLD;
+	@FXML
+	private Slider slider;
+	@FXML
+	private Label sliderValue;
+	@FXML
+	private Button BET;
+
 	public ChoiceBar() {
 		Resources.loadFXML(this);
 		setSizes();
 	}
-	
+
 	@FXML
 	private void setSizes() {
-		slider.prefWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.3));
-        slider.setMinWidth(width * 0.15);
-        slider.setMax(5000);
-		chatField.prefWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.22));
-       
-        sliderValue.setMinWidth(width * 0.09);
-        sliderValue.setMaxWidth(height * 0.09);
+		slider.prefWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.3));
+		slider.setMinWidth(width * 0.15);
+		slider.setMax(5000);
+		chatField.prefWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.22));
+
+		sliderValue.setMinWidth(width * 0.09);
+		sliderValue.setMaxWidth(height * 0.09);
 	}
-	
+
 	@FXML
 	public void setEvents(GUIPlayer client, Player player) {
 		this.client = client;
@@ -49,75 +55,78 @@ public class ChoiceBar extends HBox {
 		FOLD.setOnAction(e -> foldAction());
 		BET.setOnAction(e -> betAction());
 		slider.valueProperty().addListener(new ChangeListener<Number>() {
-            @Override
-            public void changed(ObservableValue<? extends Number> observable,
-                                Number oldValue, Number newValue) {
-                sliderValue.textProperty().setValue(checkMaxBid(slider));
-            }
-        });
+			@Override
+			public void changed(ObservableValue<? extends Number> observable,
+					Number oldValue, Number newValue) {
+				sliderValue.textProperty().setValue(checkMaxBid(slider));
+			}
+		});
 	}
+
 	/**
 	 * This will become fxml
 	 */
-	private void foldAction(){
+	private void foldAction() {
 		client.setAction(new Action.Fold());
 	}
+
 	/**
 	 * This will become fxml
 	 */
-	private void betAction(){
-		PossibleActions pa = client.getSession()
-                .getPlayerOptions(player);
+	private void betAction() {
+		PossibleActions pa = client.getSession().getPlayerOptions(player);
 
-		 if ( pa.canRaise() && slider.getValue() > 1)
-             raise(client, slider, player);
-         else if (pa.canCall())
-             client.setAction(new Action.Call());
-         else if(pa.canCheck())
-             client.setAction(new Action.Check());
+		if (pa.canRaise() && slider.getValue() > 1)
+			raise(client, slider, player);
+		else if (pa.canCall())
+			client.setAction(new Action.Call());
+		else if (pa.canCheck())
+			client.setAction(new Action.Check());
 	}
-	
+
 	private void raise(GUIPlayer client, Slider raiseSlider, Player player) {
-        if (client.getSession().getPlayerOptions(player).getMinRaise() <= raiseSlider
-                .getValue()) {
-            if (client.getSession().getPlayerOptions(player).getMaxRaise() >= raiseSlider
-                    .getValue())
-                client.setAction(new Action.Raise((int) raiseSlider.getValue()));
-        }
-    }
+		if (client.getSession().getPlayerOptions(player).getMinRaise() <= raiseSlider
+				.getValue()) {
+			if (client.getSession().getPlayerOptions(player).getMaxRaise() >= raiseSlider
+					.getValue())
+				client.setAction(new Action.Raise((int) raiseSlider.getValue()));
+		}
+	}
+
 	/**
-     * If you raise all you have, change text of raise button to ALL IN
-     *
-     * @param slider
-     * @return
-     */
-    private String checkMaxBid(Slider slider) {
-        if (slider.getValue() == slider.getMax()) BET.setText("ALL IN");
-        else BET.setText("RAISE");
-        return (int) slider.getValue() + " CHIPS";
-    }
-    
-    /**
-     * Removes eventpossibilities and makes buttons grey, if they are not
-     * possible to click.
-     *
-     * @param player
-     */
-    public void updatePossibleBarsToClick(Player player) {
-        PossibleActions pa = player.getClient().getSession()
-                .getPlayerOptions(player);  
-        if( pa.canCall())
-        	BET.setText("Call");
-   
-        if (pa.canRaise() && slider.getValue() > 1) {
-            BET.getStyleClass().add("button");
-            BET.setText("RAISE");
-        }
-        else  if(pa.canCheck())
-        	BET.setText("Check");
-        
-        slider.setMax(pa.getMaxRaise());
-        slider.setMin(pa.getMinRaise());
-        slider.setValue(pa.getMinRaise());
-    }
+	 * If you raise all you have, change text of raise button to ALL IN
+	 *
+	 * @param slider
+	 * @return
+	 */
+	private String checkMaxBid(Slider slider) {
+		if (slider.getValue() == slider.getMax())
+			BET.setText("ALL IN");
+		else
+			BET.setText("RAISE");
+		return (int) slider.getValue() + " CHIPS";
+	}
+
+	/**
+	 * Removes eventpossibilities and makes buttons grey, if they are not
+	 * possible to click.
+	 *
+	 * @param player
+	 */
+	public void updatePossibleBarsToClick(Player player) {
+		PossibleActions pa = player.getClient().getSession()
+				.getPlayerOptions(player);
+		if (pa.canCall())
+			BET.setText("Call");
+
+		if (pa.canRaise() && slider.getValue() > 1) {
+			BET.getStyleClass().add("button");
+			BET.setText("RAISE");
+		} else if (pa.canCheck())
+			BET.setText("Check");
+
+		slider.setMax(pa.getMaxRaise());
+		slider.setMin(pa.getMinRaise());
+		slider.setValue(pa.getMinRaise());
+	}
 }
