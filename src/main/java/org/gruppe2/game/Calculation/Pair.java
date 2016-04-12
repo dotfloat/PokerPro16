e82ffe392f115f144767a40d4a36fc0c4465fe4c
INF112v16-xix004 <xix004@student.uib.no>
2016-04-12 15:05:41 +0200

    redid pair.java

--- src/main/java/org/gruppe2/game/Calculation/Pair.java
+++ src/main/java/org/gruppe2/game/Calculation/Pair.java
@@ -4,15 +4,26 @@ import org.gruppe2.game.old.Card;
 import org.gruppe2.game.old.Player;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by �smund on 11/04/2016.
  */
-public class Pair {
-    public static Boolean canGetPair(ArrayList<Card> communityCards, Player p){
+public class Pair implements HandCalculation{
+    public static Boolean canGetPair(Collection<Card> communityCards, Player p){
         int amountOfSameFace = GeneralCalculations.amountOfSameFace(communityCards, p);
         if(communityCards.size() == 5 &&  amountOfSameFace < 2)
             return false;
         return true;
+    }
+
+    @Override
+    public boolean canGetHand(Collection<Card> communityCards, Player p) {
+        return canGetPair(communityCards,p);
+    }
+
+    @Override
+    public double handProbability(Collection<Card> communityCards, Player p) {
+        return 0;
     }
 }
