887c772fae2e3a543ca197600c164670c9ca985d
INF112v16-htj063 <htj063@student.uib.no>
2016-04-12 21:41:28 +0200

    trying to get stats to work

--- src/main/java/org/gruppe2/ui/javafx/Statistics.java
+++ src/main/java/org/gruppe2/ui/javafx/Statistics.java
@@ -1,5 +1,10 @@
 package org.gruppe2.ui.javafx;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.StackPane;
@@ -12,6 +17,28 @@ public class Statistics extends StackPane {
 	
 	public Statistics() {
 		Resources.loadFXML(this);
+		setStasticsTest();
+	}
+
+	private void setStasticsTest() {
+		String name = "/testLogs/2016.103_21.17.txt";
+		System.out.println(name);
+		String everything = "";
+		try(BufferedReader br = new BufferedReader(new FileReader(name))) {
+		    StringBuilder sb = new StringBuilder();
+		    String line = br.readLine();
+
+		    while (line != null) {
+		        sb.append(line);
+		        sb.append(System.lineSeparator());
+		        line = br.readLine();
+		    }
+		    everything = sb.toString();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		statistics.setText(everything);
+		statistics.setWrapText(true);
 	}
 
 	public void ok() {
@@ -25,4 +52,8 @@ public class Statistics extends StackPane {
 	public void getStatistics(){
 		System.out.println("WHere are those GOD DAMN STATS! PRONTO!");
 	}	
+	public void replay(){
+		System.out.println("Replay STATS! PRONTO!");
+	}
+	
 }
