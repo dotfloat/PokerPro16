98aebed8c44d8325ac78936abb437f042ec98d83
INF112v16-htj063 <htj063@student.uib.no>
2016-04-12 15:15:41 +0200

    cards are now an own view

--- src/main/java/org/gruppe2/ui/javafx/GUIPlayer.java
+++ src/main/java/org/gruppe2/ui/javafx/GUIPlayer.java
@@ -31,7 +31,10 @@ public class GUIPlayer extends GameClient {
 	@Override
 	public void onRoundStart() {
 		Platform.runLater(() -> {
-			gameWindow.setPlayerCards();
+			System.out.println(gameWindow);
+			System.out.println(gameWindow.playerCards);
+			System.out.println(gameWindow.gameSession);
+			((PlayerCards) gameWindow.playerCards).setPlayerCards(gameWindow.gameSession, communityCards);
 			System.out.println("roundStartTest");
 		});
 	}
---
+++ src/main/java/org/gruppe2/ui/javafx/PlayerCards.java
@@ -0,0 +1,60 @@
+package org.gruppe2.ui.javafx;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import org.gruppe2.game.old.GameSession;
+import org.gruppe2.game.old.Player;
+import org.gruppe2.ui.Resources;
+
+public class PlayerCards extends Pane {
+	private int width = PokerApplication.getWidth();
+	private int height = PokerApplication.getHeight();
+	
+	@FXML ImageView playerCard1;
+	@FXML ImageView playerCard2;
+	
+	public PlayerCards() {
+		Resources.loadFXML(this);
+	}
+	
+	/**
+	 * This is just test method for proof of conecpt, change this when backend
+	 * is ready with playerCards.
+	 */
+	public void setPlayerCards(GameSession gameSession,CommunityCards communityCardsBox) {
+		System.out.println("hello");
+		Player player = gameSession.getPlayers().get(0);
+		playerCard1.setImage(new Image(("/images/cards/"
+				+ communityCardsBox.getCardName(player.getCard1()) + ".png")));
+		playerCard2.setImage(new Image(("/images/cards/"
+				+ communityCardsBox.getCardName(player.getCard2()) + ".png")));
+		playerCard1.setLayoutX(width * 0.80);
+		playerCard1.setLayoutY(height * 0.77);
+		playerCard2.setLayoutX(width * 0.88);
+		playerCard2.setLayoutY(height * 0.77);
+		playerCard1.layoutXProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.80));
+		playerCard1.layoutYProperty().bind(
+				PokerApplication.getRoot().heightProperty().multiply(0.77));
+		playerCard2.layoutXProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.88));
+		playerCard2.layoutYProperty().bind(
+				PokerApplication.getRoot().heightProperty().multiply(0.77));
+		playerCard1.setFitWidth(width * 0.12);
+		playerCard1.setPreserveRatio(true);
+		playerCard1.setSmooth(true);
+		playerCard1.fitWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.12));
+		playerCard2.setFitWidth(width * 0.12);
+		playerCard2.setPreserveRatio(true);
+		playerCard2.setSmooth(true);
+		playerCard2.fitWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.12));
+
+		playerCard1.setRotate(350);
+		playerCard2.setRotate(5);
+	}
+}
