b754670faefe8bb65e28aaf76c35d542ef664d5a
Håkon Knudsen <hkn003@uib.no>
2016-04-13 12:28:08 +0200

    GameBuilder now supports choosing random, advanced or mixed AIs. Mixed will add both random and advanced ai to the game.

--- src/main/java/org/gruppe2/game/old/GameBuilder.java
+++ src/main/java/org/gruppe2/game/old/GameBuilder.java
@@ -3,13 +3,16 @@ package org.gruppe2.game.old;
 import org.gruppe2.ai.AIBuilder;
 import org.gruppe2.ai.Difficulty;
 
+import java.util.Random;
+
 public class GameBuilder {
     private int numAI = 0;
     private GameClient client = null;
     private int bigBlind = 0;
     private int smallBlind = 0;
     private int startMoney = 0;
-    private Difficulty aiDifficulty = Difficulty.ADVANCED;
+    private GameBuilderAiDifficultyOptions aiDifficulty = GameBuilderAiDifficultyOptions.MIXED;
+    private Random random = new Random();
 
     public GameBuilder ai(int numAI) {
         this.numAI = numAI;
@@ -17,7 +20,7 @@ public class GameBuilder {
         return this;
     }
 
-    public GameBuilder aiDifficulty(Difficulty difficulty) {
+    public GameBuilder aiDifficulty(GameBuilderAiDifficultyOptions difficulty) {
         this.aiDifficulty = difficulty;
         return this;
     }
@@ -48,8 +51,26 @@ public class GameBuilder {
             session.addPlayer(client, startMoney);
         }
 
+        //Set difficulty for AI. There is a nicer way to do this, but deadline is coming.
+        //Will probably clean up later
         for (int i = 0; i < numAI; i++) {
-            session.addPlayer(new AIBuilder().difficulty(aiDifficulty).build(), startMoney);
+            Difficulty difficulty;
+            switch (aiDifficulty) {
+                case MIXED:
+                    difficulty = Difficulty.values()[random.nextInt(Difficulty.values().length)];
+                    break;
+                case ADVANCED:
+                    difficulty = Difficulty.ADVANCED;
+                    break;
+                case RANDOM:
+                    difficulty = Difficulty.RANDOM;
+                    break;
+                default:
+                    difficulty = Difficulty.ADVANCED;
+                    break;
+            }
+
+            session.addPlayer(new AIBuilder().difficulty(difficulty).build(), startMoney);
         }
 
         return session;
