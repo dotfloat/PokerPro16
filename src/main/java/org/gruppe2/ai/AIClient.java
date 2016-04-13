d630f412c02aa3e523e1f373e52bc916627120dc
Daniel Jensen <dje004@uib.no>
2016-04-13 16:21:13 +0200

    Ai advanced mode does not try to raise with more than possible

--- src/main/java/org/gruppe2/ai/AIClient.java
+++ src/main/java/org/gruppe2/ai/AIClient.java
@@ -12,7 +12,7 @@ import java.util.Random;
 
 public class AIClient extends GameClient {
     private static final List<String> names = Arrays.asList("Anne", "Bob", "Chuck", "Dennis", "Emma", "McGyver","NEO","LINK","ZELDA","IRON MAN","Mario","MAO","STAIL","Putin");
-    private Difficulty difficulty = Difficulty.RANDOM;
+    private Difficulty difficulty = Difficulty.ADVANCED;
 
     public AIClient() {
         Random rand = new Random();
@@ -34,6 +34,8 @@ public class AIClient extends GameClient {
         Random rand = new Random();
         ArrayList<Integer> types = new ArrayList<>();
         PossibleActions actions = getSession().getPlayerOptions(player);
+        
+        if (difficulty == Difficulty.ADVANCED){
        
         double handStrength = AIHandCalculator.getHandStrength(player.getClient().getSession().getTable(),player);
 		double bank = player.getBank();
@@ -43,54 +45,57 @@ public class AIClient extends GameClient {
 			rateOfReturn = handStrength / (toRaise / bank);
 			
 		}
-		Action act = chooseAction(rateOfReturn,actions,bank,handStrength);
+		Action act = chooseAction(rateOfReturn,actions,bank,handStrength,player);
 		return act;
+        } else if (difficulty == Difficulty.RANDOM){
 		
-//
-//        if (actions.canCall()) {
-//            for (int i = 0; i < 8; i++)
-//                types.add(call);
-//        }
-//
-//        if (actions.canCheck()) {
-//            for (int i = 0; i < 8; i++)
-//                types.add(check);
-//        }
-//
-//        if (actions.canRaise()) {
-//            for (int i = 0; i < 3; i++)
-//                types.add(raise);
-//        }
-//
-//        types.add(fold);
-//
-//        switch (types.get(rand.nextInt(types.size()))) {
-//            case call:
-//                return new Action.Call();
-//
-//            case check:
-//                return new Action.Check();
-//
-//            case raise:
-//                if (actions.getMinRaise() == actions.getMaxRaise())
-//                    return new Action.Raise(actions.getMaxRaise());
-//                int maxRaiseAmount = actions.getMaxRaise();
-//                double smartRaise = rand.nextDouble();
-//                if (smartRaise <= 0.90)
-//                    maxRaiseAmount = (int) (Math.ceil(maxRaiseAmount * 0.05));
-//                else if (smartRaise > 0.90 && smartRaise <= 0.999)
-//                    maxRaiseAmount = (int) (Math.ceil(maxRaiseAmount * 0.20));
-//                if (maxRaiseAmount == actions.getMaxRaise())
-//                    return new Action.Raise(maxRaiseAmount);
-//                return new Action.Raise(
-//                        rand.nextInt(maxRaiseAmount - actions.getMinRaise()) + actions.getMinRaise());
-//
-//            default:
-//                return new Action.Fold();
-//        }
+
+        if (actions.canCall()) {
+            for (int i = 0; i < 8; i++)
+                types.add(call);
+        }
+
+        if (actions.canCheck()) {
+            for (int i = 0; i < 8; i++)
+                types.add(check);
+        }
+
+        if (actions.canRaise()) {
+            for (int i = 0; i < 3; i++)
+                types.add(raise);
+        }
+
+        types.add(fold);
+
+        switch (types.get(rand.nextInt(types.size()))) {
+            case call:
+                return new Action.Call();
+
+            case check:
+                return new Action.Check();
+
+            case raise:
+                if (actions.getMinRaise() == actions.getMaxRaise())
+                    return new Action.Raise(actions.getMaxRaise());
+                int maxRaiseAmount = actions.getMaxRaise();
+                double smartRaise = rand.nextDouble();
+                if (smartRaise <= 0.90)
+                    maxRaiseAmount = (int) (Math.ceil(maxRaiseAmount * 0.05));
+                else if (smartRaise > 0.90 && smartRaise <= 0.999)
+                    maxRaiseAmount = (int) (Math.ceil(maxRaiseAmount * 0.20));
+                if (maxRaiseAmount <= actions.getMaxRaise())
+                    return new Action.Raise(maxRaiseAmount);
+                return new Action.Raise(
+                        rand.nextInt(maxRaiseAmount - actions.getMinRaise()) + actions.getMinRaise());
+
+            default:
+                return new Action.Fold();
+        }
+        }
+        return new Action.Fold();
     }
 
-    public Action chooseAction(double rateOfReturn, PossibleActions actions, double bank, double handStrength) {
+    public Action chooseAction(double rateOfReturn, PossibleActions actions, double bank, double handStrength, Player player) {
         Random r = new Random();
         int random = r.nextInt(100) + 1;
         
@@ -113,7 +118,7 @@ public class AIClient extends GameClient {
                 return new Action.Check();
             if (random > 95) {
                 if (actions.canRaise()) {
-                    if (bank > 10 * this.getSession().getBigBlindAmount())
+                    if (actions.getMaxRaise() > 10 * this.getSession().getBigBlindAmount())
                         return new Action.Raise(this.getSession().getBigBlindAmount());
                 }
             } else
@@ -125,7 +130,7 @@ public class AIClient extends GameClient {
                 return new Action.Check();
             }
             if (random > 80 && random <= 95) {
-                if (actions.canRaise() && bank > 8 * this.getSession().getBigBlindAmount()) {
+                if (actions.canRaise() && actions.getMaxRaise() > 8 * this.getSession().getBigBlindAmount()) {
                     return new Action.Raise(this.getSession().getBigBlindAmount());
                 }
             }
@@ -139,7 +144,7 @@ public class AIClient extends GameClient {
         } else if (rateOfReturn <= 1.3) {
             if (random <= 60 && actions.canCall()) {
                 return new Action.Call();
-            } else if (actions.canRaise() && bank > this.getSession().getBigBlindAmount() * 3) {
+            } else if (actions.canRaise() && actions.getMaxRaise() > this.getSession().getBigBlindAmount() * 3) {
                 return new Action.Raise(this.getSession().getBigBlindAmount() * 2);
             }
             
@@ -150,11 +155,14 @@ public class AIClient extends GameClient {
                     return new Action.Call();
             } else {
                 if (actions.canRaise()) {
-                    int numberOfBigBlinds = (int) (bank / this.getSession().getBigBlindAmount());
+                    int numberOfBigBlinds = actions.getMaxRaise() / this.getSession().getBigBlindAmount());
                     int randomBlinds = r.nextInt(numberOfBigBlinds) + 1;
                     return new Action.Raise(this.getSession().getBigBlindAmount() * randomBlinds);
                 } else if (actions.canCall())
                     return new Action.Call();
+                else if (actions.canCheck()){
+                	return new Action.Check();
+                }
                 return new Action.Fold();
             }
         }
