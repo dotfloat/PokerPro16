1b0bec595ec92cb90b08b9de20b72f169db033c7
Petter <petterthorgersen@hotmail.com>
2016-04-13 18:30:39 +0200

    Better positioning of communitycards

--- src/main/java/org/gruppe2/ui/javafx/Table.java
+++ src/main/java/org/gruppe2/ui/javafx/Table.java
@@ -2,22 +2,25 @@ package org.gruppe2.ui.javafx;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
+import javafx.scene.layout.VBox;
+import org.gruppe2.game.old.GameSession;
 import org.gruppe2.ui.Resources;
 
 public class Table extends StackPane {
 
-	@FXML
-	private ImageView pokerTable;
-	@FXML
-	public ChatBox chatBox;
-	@FXML
-	private RightMenu rightMenu;
-	@FXML
-	public CommunityCards communityCardsBox;
-	
+	@FXML private ImageView pokerTable;
+	@FXML private VBox tableItemsBox;
+	@FXML public ChatBox chatBox;
+	@FXML private RightMenu rightMenu;
+	@FXML public CommunityCards communityCardsBox;
+	@FXML private Pot pot;
+	@FXML private Label nothing;
+	@FXML private Label nothing2;
 
 	@SuppressWarnings("static-access")
 	public Table() {
@@ -26,5 +29,20 @@ public class Table extends StackPane {
 				PokerApplication.getRoot().widthProperty().multiply(0.6));
 		this.setAlignment(rightMenu, Pos.TOP_RIGHT);
 		this.setAlignment(chatBox, Pos.BOTTOM_LEFT);
+		sizeTableBox();
+	}
+
+	private void sizeTableBox() {
+		double scale = 0.075;
+		tableItemsBox.spacingProperty().bind(PokerApplication.getRoot().heightProperty().multiply(0.002));
+		communityCardsBox.minHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		communityCardsBox.maxHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		pot.minHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		pot.maxHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		nothing.minHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		nothing.maxHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		nothing2.minHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+		nothing2.maxHeightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(scale));
+
 	}
 }
