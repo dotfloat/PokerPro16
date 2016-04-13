07cd507c6ce9ce52430df61ed25e91ee3cdb95f7
INF112v16-rse005 <rse005@student.uib.no>
2016-04-13 13:15:46 +0200

    Added a test class for AIClient.java

--- src/test/java/org/gruppe2/ai/BotTest.java
+++ src/test/java/org/gruppe2/ai/BotTest.java
@@ -1,43 +1,55 @@
 package org.gruppe2.ai;
 
-import org.gruppe2.game.old.ShowdownEvaluator;
-import org.gruppe2.game.old.ShowdownEvaluator.Hand;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import org.gruppe2.game.old.Card;
+import org.gruppe2.game.old.Player;
 import org.junit.Test;
+import org.gruppe2.game.old.Action;
+import org.gruppe2.game.old.Card.Suit;
 
 public class BotTest {
 
     @Test
-    public void testTest() {
-        ShowdownEvaluator sh = new ShowdownEvaluator();
-        for (Hand h : sh.reverse(Hand.values())) {
-            System.out.println(h);
+    public void botShouldNotFoldWithGoodStartingHand() {
+    	MockGameSession mockSession = new MockGameSession();
+    	AIMockTable table = new AIMockTable();
+//    	table.setCard(new Card(14, Suit.CLUBS));
+//    	table.setCard(new Card(14, Suit.SPADES));
+    	mockSession.setTable(table);
+    	ArrayList<Player> myPlayers = new ArrayList<>();
+    	
+    	// Generate bot test subject:
+    	AIMockPlayer player = new AIMockPlayer("MY TEST BOT");
+    	myPlayers.add(player);
+    	player.setCards(new Card(14, Suit.HEARTS), new Card(14,Suit.DIAMONDS));
+        
+        // Generate opponents:
+        int amountOfOpponents = 4;
+        for(int i = 0; i < amountOfOpponents; i++) {
+        	AIMockPlayer p = new AIMockPlayer("Opponent " + (i+1));
+        	myPlayers.add(p);
+        	Random r = new Random();
+        	p.setCards(new Card(r.nextInt(13)+2,Suit.CLUBS), new Card(r.nextInt(13)+2,Suit.HEARTS));
+//        	AIMockClient mock = new AIMockClient(p, false);
+//        	mock.setSession(mockSession);
+        	p.getClient().setSession(mockSession);
+        }
+        
+        for(Player p : myPlayers) {
+        	p.getClient().setSession(mockSession);
+        }
+        
+        for(Player p : myPlayers) {
+        	if(p.getName().equals("MY TEST BOT"))
+        		assertTrue(!p.getClient().onTurn(p).getClass().equals(new Action.Fold().getClass()));
+//        	System.out.println(p.getClient().onTurn(p));
+//        	System.out.println("Player " + p.getName() + " has cards:");
+//        	System.out.println(p.getCard1());
+//        	System.out.println(p.getCard2());
         }
     }
-
-    // TODO: Fix test
-//    @Test
-//    public void dontFoldOnRoyalFlush() {
-//        Table table = new PokerTableMock();
-//        Bot bot = new Bot(100, table);
-//        bot.giveCards(new Card(10, Card.Suit.CLUBS), new Card(11, Card.Suit.CLUBS));
-//
-//        assertNotEquals(Action.Type.FOLD, bot.onTurn().getType());
-//    }
 }
-
-//class PokerTableMock extends Table {
-//    public PokerTableMock() {
-//        super(null, 0);
-//    }
-//
-//    @Override
-//    public ArrayList<Card> getCardOnTable() {
-//        ArrayList<Card> cards = new ArrayList<>();
-//        cards.add(new Card(7, Card.Suit.HEARTS));
-//        cards.add(new Card(12, Card.Suit.CLUBS));
-//        cards.add(new Card(13, Card.Suit.CLUBS));
-//        cards.add(new Card(14, Card.Suit.CLUBS)); //This is Ace
-//        cards.add(new Card(2, Card.Suit.SPADES));
-//        return cards;
-//    }
-//}
