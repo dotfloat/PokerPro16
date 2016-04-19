c8ecafa4865b16283a4a2acb5bf4f6c1dfc76667
Petter <petterthorgersen@hotmail.com>
2016-04-19 13:55:12 +0200

    displays raining pokerpro money on intro screen

--- src/main/java/org/gruppe2/ui/javafx/ConfettiOrMoney.java
+++ src/main/java/org/gruppe2/ui/javafx/ConfettiOrMoney.java
@@ -4,6 +4,8 @@ import javafx.animation.*;
 import javafx.beans.NamedArg;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.LineTo;
@@ -16,15 +18,29 @@ import org.gruppe2.ui.Resources;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by Petter on 18/04/2016. This class creates falling confetti! Oh joy.
  */
-public class Confetti extends Pane {
+public class ConfettiOrMoney extends Pane {
 
-    public Confetti(@NamedArg("size") int size) {
+    public ConfettiOrMoney(@NamedArg("size") int size, @NamedArg("isConfetti") boolean value) {
         Resources.loadFXML(this);
-        getChildren().addAll(fallingNodes(size));
+        if (value) getChildren().addAll(fallingNodes(size));
+        else getChildren().addAll(fallingMoney(size));
+    }
+
+
+    private List<Node> fallingMoney(int size) {
+        ArrayList<Node> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            ImageView money = createMoney();
+            fallingAnimation(money);
+            setFlipAnimation(money);
+            list.add(money);
+        }
+        return list;
     }
 
     private List<Node> fallingNodes(int size) {
@@ -32,7 +48,6 @@ public class Confetti extends Pane {
         for (int i = 0; i < size; i++) {
             Node node = drawRectangle();
             fallingAnimation(node);
-            setFlipAnimation(node);
             list.add(node);
         }
         setRotateAnimation(list);
@@ -44,8 +59,13 @@ public class Confetti extends Pane {
         double startY = -Math.random() * PokerApplication.getHeight();
         Path path = new Path(new MoveTo(startX, startY), new LineTo(startX, PokerApplication.getHeight()));
         PathTransition pathTransition = new PathTransition(Duration.millis(Math.random() * 5000 + 2000), path, node);
-        pathTransition.setCycleCount(Timeline.INDEFINITE);
         pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        if (node instanceof Rectangle) {
+            pathTransition.setCycleCount(1);
+            pathTransition.setOnFinished(finishHim -> {
+                getChildren().remove(node);
+            });
+        } else pathTransition.setCycleCount(Timeline.INDEFINITE);
         pathTransition.play();
     }
 
@@ -54,14 +74,14 @@ public class Confetti extends Pane {
             RotateTransition rt = new RotateTransition(Duration.millis(Math.random() * 3000 + 2000), node);
             if (Math.random() > 0.5) rt.setByAngle(-360);
             else rt.setByAngle(360);
-            rt.setCycleCount(Animation.INDEFINITE);
             rt.setDelay(Duration.ZERO);
+            rt.setCycleCount(Animation.INDEFINITE);
             rt.play();
         }
     }
 
-    private void setFlipAnimation(Node node){
-        RotateTransition rotator = new RotateTransition(Duration.millis(500), node);
+    private void setFlipAnimation(Node node) {
+        RotateTransition rotator = new RotateTransition(Duration.millis(node.opacityProperty().get() * 3700 + 300), node);
         rotator.setAxis(Rotate.X_AXIS);
         rotator.setFromAngle(0);
         rotator.setToAngle(360);
@@ -74,6 +94,19 @@ public class Confetti extends Pane {
         double opacity = Math.random();
         Color color = new Color(Math.random(), Math.random(), Math.random(), opacity);
         Rectangle rectangle = new Rectangle(opacity * 30, opacity * 10, color);
+        System.out.println(rectangle.getOpacity());
+        rectangle.widthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.05 * opacity));
+        rectangle.heightProperty().bind(PokerApplication.getRoot().heightProperty().multiply(0.02 * opacity));
         return rectangle;
+    }
+
+    private ImageView createMoney() {
+        double opacity = ThreadLocalRandom.current().nextDouble(0.2, 1);
+        if (Math.random() > 0.98) opacity = 0.99;
+        ImageView money = new ImageView(new Image("/images/ui/moneyS.png"));
+        money.opacityProperty().setValue(opacity + 0.2);
+        money.preserveRatioProperty().setValue(true);
+        money.fitWidthProperty().bind(PokerApplication.getRoot().widthProperty().multiply(0.1 * opacity));
+        return money;
     }
 }
