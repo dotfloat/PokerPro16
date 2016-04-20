dffb74d5936b14b8bb31785f79a9e38d4194d3c6
INF112v16-htj063 <htj063@student.uib.no>
2016-04-20 17:23:06 +0200

    game can now read settings, but not put new ones, look ugly, someone fix layout on buttons if they want to

--- src/main/java/org/gruppe2/ui/javafx/Settings.java
+++ src/main/java/org/gruppe2/ui/javafx/Settings.java
@@ -1,7 +1,13 @@
 package org.gruppe2.ui.javafx;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 
@@ -17,25 +23,82 @@ public class Settings extends StackPane {
 	@FXML private TextField  avatar;
 	@FXML private TextField  numberBots;
 	@FXML private Button botButton;
+	@FXML private Label nameLabel;
+	@FXML private Label smallLabel;
+	@FXML private Label bigLabel;
+	@FXML private Label startValueLabel;
+	@FXML private Label avatarLabel;
+	@FXML private Label numberBotsLabel;
 	
 	public Settings() {
 		Resources.loadFXML(this);
+		loadSettings();
+	}
+	
+	
+	
+
+	private void loadSettings() {
 		if(PokerApplication.diff == GameBuilderAiDifficultyOptions.RANDOM)
 			botButton.setText("BOT IS EASY");
 		else if(PokerApplication.diff == GameBuilderAiDifficultyOptions.MIXED)
 			botButton.setText("Bot is MIXED");
 		else
 			botButton.setText("Bot is HARD");
+		ArrayList<String> savedSettings = getSavedSettingsList();
 		
+		nameLabel.setText(savedSettings.get(0));
+		PokerApplication.name = nameLabel.getText();
+		smallLabel.setText(savedSettings.get(1));
+		PokerApplication.small = Integer.valueOf(smallLabel.getText());
+		bigLabel.setText(savedSettings.get(2));
+		PokerApplication.big = Integer.valueOf(bigLabel.getText());
+		startValueLabel.setText(savedSettings.get(3));
+		PokerApplication.bank = Integer.valueOf(startValueLabel.getText());
+		avatarLabel.setText(savedSettings.get(4));
+		numberBotsLabel.setText(savedSettings.get(5));
+		if(numberBotsLabel.getText().matches("[0-9]+") && numberBotsLabel.getText().length() > 0 && Integer.valueOf(numberBotsLabel.getText()) < 10) {
+			int numBots = Integer.valueOf(numberBotsLabel.getText());
+
+			if (numBots < 3)
+				numBots = 3;
+			else if (numBots > 9)
+				numBots = 9;
+
+			PokerApplication.numberOfBots = numBots;
+		}
 	}
 
-	
+
+
+
+	private ArrayList<String> getSavedSettingsList() {
+		ArrayList<String> savedSettings = new ArrayList<String>();
+		try {
+			
+			BufferedReader brq = new BufferedReader(new FileReader("src/main/resources/saved_files/settings/standardSettings.txt"));
+			
+			String currentLine;
+			
+			while ((currentLine = brq.readLine()) != null) {
+				savedSettings.add(currentLine);
+			}
+			brq.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return savedSettings;
+	}
+
+
+
 
 	public void ok() {
-		setStartMoney();
 		setName();
 		setSmallBlind();
 		setBigBlind();
+		setStartMoney();
 		setAvatar();
 		numberOfBots();
 		SceneController.setScene(new MainMenu());
