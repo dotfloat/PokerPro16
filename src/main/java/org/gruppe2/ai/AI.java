e8cd11625f712ae6458f581cb5184bc4aca638c3
INF112v16-mna011 <miknau94@gmail.com>
2016-04-21 13:47:00 +0200

    Actually added the new classes

---
+++ src/main/java/org/gruppe2/ai/AI.java
@@ -0,0 +1,12 @@
+package org.gruppe2.ai;
+
+import org.gruppe2.game.Action;
+import org.gruppe2.game.model.PlayerModel;
+import org.gruppe2.game.model.RoundPlayerModel;
+
+/**
+ * Created by Mikal on 21.04.2016.
+ */
+public interface AI {
+    public void doAction(PlayerModel playerModel);
+}
