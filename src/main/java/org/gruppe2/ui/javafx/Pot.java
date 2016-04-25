a420d09da036726ded1108e7812ee6c45d3d8592
INF112v16-htj063 <htj063@student.uib.no>
2016-04-25 13:37:34 +0200

    game now scales much better on different res

--- src/main/java/org/gruppe2/ui/javafx/Pot.java
+++ src/main/java/org/gruppe2/ui/javafx/Pot.java
@@ -1,22 +1,36 @@
 package org.gruppe2.ui.javafx;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.text.Font;
 
 import org.gruppe2.ui.Resources;
 
 public class Pot extends Label {
 
-//	@FXML
-//	private Label potLabel;
-
+	static ObjectProperty<Font> fontTracking = new SimpleObjectProperty<Font>(Font.getDefault());
 	
 	public Pot() {
 		Resources.loadFXML(this);
-//		this.maxWidthProperty().bind(
-//				PokerApplication.getRoot().widthProperty().multiply(0.2));
 		
+		setBindings();
 	}
+	private void setBindings() {
+		this.maxWidthProperty().bind(
+				PokerApplication.getRoot().widthProperty().multiply(0.2));
+		this.fontProperty().bind(fontTracking);
+		this.widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldWidth, Number newWidth) {
+                fontTracking.set(Font.font(newWidth.doubleValue() / 12));
+            }
+        });
+	
+}
 	public void updatePot(int value){
 		this.setText("Total Pot: "+String.valueOf(value)+" CHIPS");
 	}
