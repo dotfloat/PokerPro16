ae902fbe597fb7830f2d6579a3cc7dd63ad19d4c
Petter <petterthorgersen@hotmail.com>
2016-04-26 14:15:34 +0200

    Added tooltip view for statistics. Now works. Need to be connected

--- src/main/java/org/gruppe2/ui/javafx/SceneController.java
+++ src/main/java/org/gruppe2/ui/javafx/SceneController.java
@@ -4,6 +4,7 @@ import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
@@ -24,10 +25,11 @@ class SceneController {
         stage.getChildren().add(node);
     }
 
-    public static void setModal(Node node, double x, double y) {
-        if (x > PokerApplication.getRoot().getWidth() * 0.8) {
-            x *= 0.8;
-        }
-        stage.getChildren().add(node);
+    public static void setTooltip(Node node, double x, double y) {
+        if (x > PokerApplication.getRoot().getWidth() * 0.8) x *= 0.8;
+        Pane pane = (Pane) node;
+        pane.getChildren().get(0).setLayoutY(y);
+        pane.getChildren().get(0).setLayoutX(x);
+        stage.getChildren().add(new ToolTip(pane));
     }
 }
