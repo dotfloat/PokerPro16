dac6c422d6bef366f8402ceaf006507a73993567
INF112v16-htj063 <htj063@student.uib.no>
2016-04-26 11:11:57 +0200

    chatbox now almist works

--- src/main/java/org/gruppe2/game/session/HostSession.java
+++ src/main/java/org/gruppe2/game/session/HostSession.java
@@ -1,16 +1,18 @@
 package org.gruppe2.game.session;
 
-import org.gruppe2.game.controller.AIController;
-import org.gruppe2.game.controller.GameController;
-import org.gruppe2.game.controller.PlayerController;
-import org.gruppe2.game.controller.RoundController;
-import org.gruppe2.game.model.GameModel;
-import org.gruppe2.game.model.RoundModel;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+
+import org.gruppe2.game.controller.AIController;
+import org.gruppe2.game.controller.ChatController;
+import org.gruppe2.game.controller.GameController;
+import org.gruppe2.game.controller.PlayerController;
+import org.gruppe2.game.controller.RoundController;
+import org.gruppe2.game.model.ChatModel;
+import org.gruppe2.game.model.GameModel;
+import org.gruppe2.game.model.RoundModel;
 
 public class HostSession extends Session {
     private final List<PlayerController> players = Collections.synchronizedList(new ArrayList<>());
@@ -18,6 +20,7 @@ public class HostSession extends Session {
     public HostSession(Integer minPlayers, Integer maxPlayers, Integer buyIn, GameModel.BotPolicy botPolicy) {
         addModel(new GameModel(UUID.randomUUID(), minPlayers, maxPlayers, buyIn, botPolicy));
         addModel(new RoundModel());
+        addModel(new ChatModel());
     }
 
     @Override
@@ -25,6 +28,7 @@ public class HostSession extends Session {
         addController(GameController.class);
         addController(RoundController.class);
         addController(AIController.class);
+        addController(ChatController.class);
     }
 
     @Override
