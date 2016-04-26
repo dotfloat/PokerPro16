c71980683ab6d409441598e74bed728508b91f23
INF112v16-rse005 <rse005@student.uib.no>
2016-04-26 15:50:41 +0200

    Changed from Collection to List

--- src/main/java/org/gruppe2/game/calculation/HandCalculation.java
+++ src/main/java/org/gruppe2/game/calculation/HandCalculation.java
@@ -5,35 +5,36 @@ import org.gruppe2.game.Hand;
 
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.List;
 
-interface HandCalculation extends Comparator<Collection<Card>> {
+interface HandCalculation extends Comparator<List<Card>> {
     /**
      * Check if the given list of cards is a certain hand (ie. if it's a Pair)
      * @param cards a collection of 7 cards
      * @return true if it's a valid hand, false otherwise
      */
-    boolean isHand(Collection<Card> cards);
+    boolean isHand(List<Card> cards);
 
     /**
      * Get the best 5 cards out of the 7 available
      * @param cards a list with 7 cards
      * @return A list with 5 elements or null if it's impossible to get the hand
      */
-    Collection<Card> getBestCards(Collection<Card> cards);
+    Collection<Card> getBestCards(List<Card> cards);
 
     /**
      * Checks if its possible to get a certain hand when there are < 7 cards
      * @param cards a collection of [5, 7) cards to evaluate
      * @return true if a hand is possible to get, false otherwise
      */
-    boolean canGet(Collection<Card> cards);
+    boolean canGet(List<Card> cards);
 
     /**
      * Calculates the probability that player p gets a specific hand
      * @param player The player model for the round
      * @return The probability as a double in the range 0..1
      */
-    double probability(Collection<Card> cards);
+    double probability(List<Card> cards);
 
     /**
      * @return The hand type
