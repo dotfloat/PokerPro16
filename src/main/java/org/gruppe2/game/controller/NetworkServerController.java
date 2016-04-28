edec58875eb53c9176576df5e1329701b98b77b4
Zohar Malamant <dotfloat@gmail.com>
2016-04-28 15:56:34 +0200

    Push chat messages to clients

--- src/main/java/org/gruppe2/game/controller/NetworkServerController.java
+++ src/main/java/org/gruppe2/game/controller/NetworkServerController.java
@@ -6,13 +6,18 @@ import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.UUID;
 
+import org.gruppe2.game.event.ChatEvent;
+import org.gruppe2.game.session.Handler;
 import org.gruppe2.game.session.Message;
 
 
 public class NetworkServerController extends AbstractController{
 	ServerSocketChannel serverSocket;
 	ArrayList<SocketChannel> clients = new ArrayList<SocketChannel>();
+
 	@Override
 	public void update(){
 		if(serverSocket != null){
@@ -27,19 +32,10 @@ public class NetworkServerController extends AbstractController{
 				e.printStackTrace();
 			}
 		}
-		
-		for(SocketChannel client : clients){
-			ByteBuffer buff = ByteBuffer.allocate(100);
-			buff.clear();
-			buff.put("hello World!!<3".getBytes());
-			buff.flip();
-			try {
-				client.write(buff);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			
-		}
+
+        for (int i = 0; i < clients.size(); i++) {
+            /* Listen to what the assholes say */
+        }
 	}
 	
 	@Message
@@ -48,13 +44,33 @@ public class NetworkServerController extends AbstractController{
 			serverSocket = ServerSocketChannel.open();
 			serverSocket.socket().bind(new InetSocketAddress(8888));
 			serverSocket.configureBlocking(false);
-			
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+    @Handler
+    public void onChat(ChatEvent event) {
+        sendToAll("CHAT " + event.getPlayerUUID() + " :" + event.getMessage() + "\r\n");
+    }
+
+    private void sendToAll(String mesg) {
+        ByteBuffer buf = ByteBuffer.allocate(mesg.length());
+        buf.clear();
+        buf.put(mesg.getBytes());
+        buf.flip();
+
+        for (int i = 0; i < clients.size(); i++) {
+            SocketChannel client = clients.get(i);
+
+            try {
+                client.write(buf);
+            } catch (IOException e) {
+                clients.remove(i--);
+            }
+        }
+    }
 }
 
 
