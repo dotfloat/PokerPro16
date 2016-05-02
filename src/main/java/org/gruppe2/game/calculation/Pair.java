e32cca3be599ae5152788a995ac3eaa192be18e8
INF112v16-rse005 <rse005@student.uib.no>
2016-05-02 15:43:20 +0200

    Description change in Pair.java

--- src/main/java/org/gruppe2/game/calculation/Pair.java
+++ src/main/java/org/gruppe2/game/calculation/Pair.java
@@ -39,7 +39,7 @@ class Pair implements HandCalculation {
     }
 
     /**
-	 * Assumes both o1 and o2 are pairs.
+	 * Assumes both o1 and o2 are pairs excluding Highcards.
 	 * @return int (1, 0, -1).
 	 */
     @Override
