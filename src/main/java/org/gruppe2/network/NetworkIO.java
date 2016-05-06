532306c6c0a86183f6590e0936d3ba3696e11042
Zohar Malamant <dotfloat@gmail.com>
2016-05-06 12:25:06 +0200

    Remove bad imports

--- src/main/java/org/gruppe2/network/NetworkIO.java
+++ src/main/java/org/gruppe2/network/NetworkIO.java
@@ -1,8 +1,5 @@
 package org.gruppe2.network;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
-import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
-
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
