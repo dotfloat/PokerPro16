66081d4d15b5b7aa5a686bfc44c4f4ee3a2aef26
Zohar Malamant <dotfloat@gmail.com>
2016-05-06 10:20:56 +0200

    Add untested object sending and receiving

--- src/main/java/org/gruppe2/network/NetworkIO.java
+++ src/main/java/org/gruppe2/network/NetworkIO.java
@@ -1,5 +1,8 @@
 package org.gruppe2.network;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
@@ -33,6 +36,12 @@ public class NetworkIO {
         channel.write(buffer);
     }
 
+    /**
+     * Attempts to read a string that ends with "\r\n" and returns a split array of arguments
+     *
+     * @return Array of arguments or null if unsuccessful
+     * @throws IOException
+     */
     public String[] readMessage() throws IOException {
         fillBuffer();
 
@@ -60,6 +69,54 @@ public class NetworkIO {
         System.out.printf("Received Message: [%s]\n", message);
 
         return splitMessage(message);
+    }
+
+    public void sendObject(Object object) throws IOException {
+        ByteOutputStream byteOutputStream = new ByteOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteOutputStream);
+        objectOutputStream.writeObject(object);
+
+        byte[] bytes = byteOutputStream.getBytes();
+        ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length + 4);
+        byteBuffer.clear();
+        byteBuffer.putInt(bytes.length);
+        byteBuffer.put(bytes);
+        byteBuffer.flip();
+
+        channel.write(byteBuffer);
+
+        System.out.printf("Sent Object: [%s]\n", object.getClass());
+    }
+
+    public Object readObject() throws IOException, ClassNotFoundException {
+        fillBuffer();
+
+        if (inputBuffer.size() <= 0)
+            return null;
+
+        byte[] bytes = inputBuffer.toByteArray();
+
+        ByteBuffer lengthBuffer = ByteBuffer.allocate(4);
+        lengthBuffer.clear();
+        lengthBuffer.put(bytes, 0, 4);
+        lengthBuffer.flip();
+
+        int length = lengthBuffer.getInt();
+
+        if (length + 4 >= bytes.length) // Not enough data
+            return null;
+
+        ByteInputStream byteInputStream = new ByteInputStream(bytes, 4, length);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteInputStream);
+
+        inputBuffer.reset();
+        inputBuffer.write(bytes, length + 4, bytes.length - (length + 4));
+
+        Object object = objectInputStream.readObject();
+
+        System.out.printf("Received Object: [%s]\n", object.getClass());
+
+        return object;
     }
 
     private void fillBuffer() throws IOException {
