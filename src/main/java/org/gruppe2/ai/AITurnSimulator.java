bf5427db04d790d0ce04c4f4e0b6977ab47d7ec9
Daniel Jensen <dje004@uib.no>
2016-05-09 17:26:57 +0200

    Added AITurnSimulator

---
+++ src/main/java/org/gruppe2/ai/AITurnSimulator.java
@@ -0,0 +1,12 @@
+package org.gruppe2.ai;
+
+import java.util.List;
+
+import org.gruppe2.game.Card;
+import org.gruppe2.game.RoundPlayer;
+
+public class AITurnSimulator {
+	public double getHandStregth(RoundPlayer roundPlayer, List<Card> communityCards){
+		return 0;
+	}
+}
