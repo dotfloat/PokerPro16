5bbb141e166457ace51d0544738afa03273740c2
Håkon Knudsen <hkn003@uib.no>
2016-05-09 17:36:51 +0200

    Added a new parameter to getHandStrength. Also added some javadoc

--- src/main/java/org/gruppe2/ai/AITurnSimulator.java
+++ src/main/java/org/gruppe2/ai/AITurnSimulator.java
@@ -6,7 +6,15 @@ import org.gruppe2.game.Card;
 import org.gruppe2.game.RoundPlayer;
 
 public class AITurnSimulator {
-	public double getHandStregth(RoundPlayer roundPlayer, List<Card> communityCards){
+	/**
+	 * Simulates a number of rounds with the current AI hand. Returns a hand strength
+	 * which can be used to decide which actions to perform
+	 * @param roundPlayer
+	 * @param communityCards
+	 * @param numberOfRounds number of rounds to simulate
+     * @return
+     */
+	public double getHandStregth(RoundPlayer roundPlayer, List<Card> communityCards, int numberOfRounds){
 		return 0;
 	}
 }
