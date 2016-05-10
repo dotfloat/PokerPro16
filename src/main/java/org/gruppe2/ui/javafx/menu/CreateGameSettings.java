034ba46e1a2efa8ce7a230602610c752e17e0810
Petter <petterthorgersen@hotmail.com>
2016-05-10 12:05:39 +0200

    created settings when creating a game

--- src/main/java/org/gruppe2/ui/javafx/menu/CreateGameSettings.java
+++ src/main/java/org/gruppe2/ui/javafx/menu/CreateGameSettings.java
@@ -1,7 +1,23 @@
 package org.gruppe2.ui.javafx.menu;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import org.gruppe2.ui.UIResources;
 
 public class CreateGameSettings extends VBox {
+    @FXML
+    TextField tableName;
+    @FXML
+    TextField smallBlind;
+    @FXML
+    TextField bigBlind;
+    @FXML
+    TextField startMoney;
+    @FXML
+    TextField maxPlayers;
 
+    public CreateGameSettings(){
+        UIResources.loadFXML(this);
+    }
 }
