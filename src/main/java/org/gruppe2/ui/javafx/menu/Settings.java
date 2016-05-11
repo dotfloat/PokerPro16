71ca896386694c3b2f560ebfbf8d472425e1abbd
INF112v16-xix004 <xix004@student.uib.no>
2016-05-11 11:26:55 +0200

    added name and avatar customisation in settings

--- src/main/java/org/gruppe2/ui/javafx/menu/Settings.java
+++ src/main/java/org/gruppe2/ui/javafx/menu/Settings.java
@@ -1,14 +1,28 @@
 package org.gruppe2.ui.javafx.menu;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
 
+import org.gruppe2.Main;
+import org.gruppe2.Resources;
 import org.gruppe2.ui.UIResources;
 import org.gruppe2.ui.javafx.Modal;
 
 public class Settings extends VBox {
-	
+
+	@FXML
+	TextField nameField;
+
+	@FXML
+	TilePane avatarTiles;
+
 	public Settings(){
 		UIResources.loadFXML(this);
+		getAvatars();
 	}
 
 	public static void show() {
@@ -16,5 +30,22 @@ public class Settings extends VBox {
 		modal.setPercentSize(0.5, 0.5);
 		modal.setContent(new Settings());
 		modal.show();
+	}
+
+	public void setName(){
+		if(nameField.getText() != null && !nameField.getText().isEmpty())
+		Main.setProperty("name",nameField.getText());
+	}
+
+	private void getAvatars(){
+		String[] avatars = Resources.listAvatars();
+
+		for(String avatar: avatars){
+			//System.out.println(avatar);
+			Pane avatarPane = new Pane();
+			avatarPane.setOnMouseClicked(frisk -> Main.setProperty("avatar",avatar));
+			avatarPane.getChildren().add(new ImageView(UIResources.getAvatar(avatar)));
+			avatarTiles.getChildren().add(avatarPane);
+		}
 	}
 }
